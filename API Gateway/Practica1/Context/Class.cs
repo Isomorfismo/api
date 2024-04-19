a7b0d47d8d3b8b1b940466bbe460d95294e82ef0
Isomorfismo <118367748+Isomorfismo@users.noreply.github.com>
2024-04-18 22:13:21 -0600

    Add files via upload

---
+++ API Gateway/Practica1/Context/Class.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practica1.Context
+{
+    public class Ctxtdb:DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder buildier)
+        {
+            buildier.UseMySQL("server=localhost;database=animales;user=clases;password=123456");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<MODEL>(entity => {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name);
+                entity.Property(e => e.Description);
+                entity.Property(e => e.Family);
+                entity.HasOne(e => e.Zone).WithMany(p => p.Animales);
+            });
+        }
+    }
+}
